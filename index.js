6a055c3d9a39fe2fa9081052b1ebb8f632b43958
Nghia Truong <chickyfam2012@gmail.com>
2019-03-28 11:09:05 +0700

    remove unused code

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+/* eslint-disable import/imports-first */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/no-extraneous-dependencies */
 /**
  * @format
  */
 
 import { AppRegistry } from 'react-native';
-import App from './App';
 import { name as appName } from './app.json';
 import ChickyShop from 'ChickyShop/src/ChickyShop';
 
