059d6e63fdf8e5bcefd1c2c9006b8ced7cf9ef5a
Nghia Truong <chickyfam2012@gmail.com>
2019-03-29 11:45:49 +0700

    remove unused code

--- src/ChickyShop.js
+++ src/ChickyShop.js
@@ -1,5 +1,6 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/no-extraneous-dependencies */
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
 import AppContainer from 'ChickyShop/src/Navigators/InitialNavigator';
 
 export default class ChickyShop extends Component {
